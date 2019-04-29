d0376d4b468174b3d2aa523548c9f012c19bd053
rh01 <1048157315@qq.com>
2019-04-29 17:12:10 +0800

    add new file mcmc.

--- js/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(window).on("scroll", function() {
-      if ($(window).scrollTop() >= 20) {
-        $(".navbar").addClass("compressed");
-      } else {
-        $(".navbar").removeClass("compressed");
-      }
-    });
-  });
